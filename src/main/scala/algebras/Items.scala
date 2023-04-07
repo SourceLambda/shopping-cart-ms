97c3fd78ce7974bc56f4e4403c7833632b31e784
Hombre-x <gabisanpar@gmail.com>
2023-04-06 22:59:40 -0500

    Added the add method to the Items algebra
    Now it can persist an Item in Postgres

--- src/main/scala/algebras/Items.scala
+++ src/main/scala/algebras/Items.scala
@@ -16,6 +16,7 @@ import cats.effect.MonadCancelThrow
 trait Items[F[_]]:
 
   def findById(itemId: ItemId): F[Option[Item]]
+  def add(item: Item): F[ItemId]
   
 end Items
 
@@ -30,6 +31,16 @@ object Items:
           ps.option(itemId)
         )
       )
+
+    override def add(item: Item): F[ItemId] =
+      postgres.use ( se =>
+        se.prepare(addItem).flatMap( cmd =>
+          cmd
+            .execute(item)
+            .as(item.id)
+            .recoverWith {case id => id.raiseError[F, ItemId]}
+        )
+      )
       
 
 private object ItemsSQL:
@@ -41,6 +52,11 @@ private object ItemsSQL:
       case i ~ n ~ d ~ p ~ bi ~ bn ~ ci ~ cn =>
         Item(i, n, d, p, Brand(bi, bn), Category(ci, cn))
     }
+    
+  val encoder: Encoder[Item] =
+    (itemId ~ itemName ~ itemDesc ~ money ~ brandId ~ brandName ~ categoryId ~ categoryName)
+      .contramap( i =>
+          i.id ~ i.name ~ i.description ~ i.price ~ i.brand.id ~ i.brand.name ~ i.category.id ~ i.category.name)
 
   val selectById: Query[ItemId, Item] =
     sql"""
@@ -48,3 +64,12 @@ private object ItemsSQL:
           FROM items AS i
           WHERE i.id = $itemId
          """.query(decoder)
+    
+  val addItem: Command[Item] =
+    sql"""
+         INSERT INTO items
+         VALUES ($itemId, $itemName, $itemDesc, $money, $brandId, $brandName, $categoryId, $categoryName)
+       """.command.contramap {
+            case i =>
+              i.id ~ i.name ~ i.description ~ i.price ~ i.brand.id ~ i.brand.name ~ i.category.id ~ i.category.name
+          }
